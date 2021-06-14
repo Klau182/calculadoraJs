081c0c49e2a841de18b0b312a5b1278e471f71e5
claudia <claudia_figueroai@hotmail.com>
2021-06-13 21:02:43 -0400

    se crean las funciones para cada boton

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,32 @@
 const botonNumeros = document.getElementsByName("data-number");
-console.log(botonNumeros);
+const botonOpera = document.getElementsByName("data-opera");
+const botonIgual = document.getElementsByName("data-igual");
+const botonDelete = document.getElementsByName("data-delete");
+//SE CREAN LAS VARIABLES CON EL NOMBRE DE LOS BOTONES
+//PARA LUEGO CREAR LAS FUNCIONES 
+var result = document.getElementById("result");
+
+//SE RECORRE CON EACH Y SE CREA UNA FUNCION LUEGO SE ENTREGA EL PARAMETRO DEL BOTON SELECCIONADO
+botonNumeros.forEach(function(boton){
+    boton.addEventListener("click", function(){
+        agregarNumero(boton.innerText)
+       
+    })
+})
+
+botonOpera.forEach(function (boton){
+    boton.addEventListener("click", function(){
+        selectOperacion(boton.innerText)
+        
+    })
+})
+
+botonIgual.addEventListener("click",function(){
+    calcular();
+    actualizarDisplay();
+});
+
+botonDelete.addEventListener("click", function(){
+    clear();
+    actualizarDisplay();
+})
